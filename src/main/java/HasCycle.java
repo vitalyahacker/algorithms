a2639e7f0c07568fb6190cbdb513e4e491a74bc9
vitalya <vitalyatrololo@mail.ru>
2021-05-14 13:47:05 +0300

    Define if singly-linked list has a cycle.

---
+++ src/main/java/HasCycle.java
@@ -0,0 +1,29 @@
+public class HasCycle {
+
+    public boolean hasCycle(ListNode head) {
+        if (head == null) return false;
+        ListNode slowPointer = head;
+        ListNode fastPointer = head.next;
+
+        while (true) {
+            if (fastPointer == null) return false;
+            if (fastPointer.next == null) return false;
+
+            if (fastPointer == slowPointer) return true;
+            if (fastPointer.next == slowPointer) return true;
+
+            slowPointer = slowPointer.next;
+            fastPointer = fastPointer.next.next;
+        }
+    }
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+}
